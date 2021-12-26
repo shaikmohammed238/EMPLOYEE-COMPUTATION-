f682a174dfc4f9fc59418eef20de9178eb1e50c4
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2021-12-26 11:35:19 +0530

    added uc9

---
+++ Uc9 EMPLOYWAGE BUILDER FOR EACH COMPANY/Uc9 EMPLOYWAGE BUILDER FOR EACH COMPANY/EMPLOY WAGE BUILD PROJECT.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uc9_EMPLOYWAGE_BUILDER_FOR_EACH_COMPANY
+{
+    class EMPLOY_WAGE_BUILD_PROJECT
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public EMPLOY_WAGE_BUILD_PROJECT(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void computeEmpWage()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingdays = 0;
+            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkingdays < this.numOfWorkingDays)
+            {
+                totalWorkingdays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingdays + "Emp Hrs:" + empHrs);
+            }
+            totalEmpWage = totalEmpHrs * this.empRatePerHour;
+            Console.WriteLine("total Emp Wage for company:" + company + "is:" + totalEmpWage);
+        }
+        public string toString()
+        {
+            return "total Emp Wage for company:" + company + " is:" + totalEmpWage;
+        }
+    }
+}
---
+++ Uc9 EMPLOYWAGE BUILDER FOR EACH COMPANY/Uc9 EMPLOYWAGE BUILDER FOR EACH COMPANY/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uc9_EMPLOYWAGE_BUILDER_FOR_EACH_COMPANY
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            EMPLOY_WAGE_BUILD_PROJECT dMart = new EMPLOY_WAGE_BUILD_PROJECT("DMart", 20, 2, 10);
+             EMPLOY_WAGE_BUILD_PROJECT reliance = new  EMPLOY_WAGE_BUILD_PROJECT("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+        }
+    }
+}
