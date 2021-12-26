c2a9bdbe7deb85acf54c0d5c8f3cce99c0ffd169
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2021-12-26 11:38:57 +0530

    added uc12

---
+++ uc12_Refactor Have A List Of MultiCompany/uc12_Refactor Have A List Of MultiCompany/CompanyEmpWage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Empwage_Using_Interface_Approach
+{
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWages;
+
+        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+
+        public void setTotalEmpWage(int totalEmpWages)
+        {
+            this.totalEmpWages = totalEmpWages;
+        }
+
+        public string toString()
+        {
+            return "Total emp wages for a company is : " + this.company + " is : " + this.totalEmpWages;
+        }
+    }
+}
---
+++ uc12_Refactor Have A List Of MultiCompany/uc12_Refactor Have A List Of MultiCompany/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using uc12_Refactor_Have_A_List_Of_MultiCompany;
+
+namespace Empwage_Using_Interface_Approach
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********Welome to Employee Wage Program***********");
+            Console.WriteLine("=====================================================================================================");
+
+            EmpWageBuilderArray empWageBuilderArray = new EmpWageBuilderArray();
+            empWageBuilderArray.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilderArray.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilderArray.addCompanyEmpWage("Samsung", 25, 5, 22);
+            empWageBuilderArray.addCompanyEmpWage("Facebook", 30, 6, 25);
+            empWageBuilderArray.computeEmpWage();
+        }
+    }
+}
