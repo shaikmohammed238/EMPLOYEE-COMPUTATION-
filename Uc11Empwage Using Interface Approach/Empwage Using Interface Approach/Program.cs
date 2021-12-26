e7acc22455885a47f964c352ad78f535ecb12252
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2021-12-26 11:38:03 +0530

    added uc11

---
+++ Uc11Empwage Using Interface Approach/Empwage Using Interface Approach/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Empwage_Using_Interface_Approach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********Welome to Employee Wage Program***********");
+            Console.WriteLine("=====================================================================================================");
+
+            EmpWageBuilderArray empWageBuilderArray = new EmpWageBuilderArray();
+            empWageBuilderArray.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilderArray.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilderArray.addCompanyEmpWage("Samsung", 25, 5, 22);
+            empWageBuilderArray.addCompanyEmpWage("Facebook", 30, 6, 25);
+            empWageBuilderArray.computeEmpWage();
+        }
+    }
+}
